2780343d2e83236cfddec204eca83cfcb1135998
agoyr <yamauchi.ryoga116@gmail.com>
2024-05-13 23:06:49 +0900

    add little bit change on skills.js

--- src/Skills.js
+++ src/Skills.js
@@ -1,13 +1,14 @@
+// Skills.js
 import React from 'react';
 import { Box, Typography, Chip } from '@mui/material';
 
 function Skills() {
-  const skills = ["HTML", "CSS", "JavaScript", "React", "Node.js"]; // Add your skills here
+  const skills = ["C", "C++","Python", "Flutter","Unity" ,"Unreal Engine","HTML", "CSS", "JavaScript", "React", "Node.js"]; // Add your skills here
 
   return (
-    <Box id="skills" sx={{ mt: 8, mb: 4 }}>
+    <Box id="skills" sx={{ mt: 8, mb: 4, mx: 'auto', maxWidth: 800, }}>
       <Typography variant="h4" gutterBottom>My Skills</Typography>
-      <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 1 }}>
+      <Box sx={{ display: 'flex', justifyContent: 'left', flexWrap: 'wrap', gap: 1 }}>
         {skills.map(skill => (
           <Chip key={skill} label={skill} variant="outlined" />
         ))}
