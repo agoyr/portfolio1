faa845a5a8472a9d353966811e42272d80726ae1
agoyr <yamauchi.ryoga116@gmail.com>
2024-05-14 12:02:27 +0900

    middle of pD

--- src/projects/ProjectDetail.js
+++ src/projects/ProjectDetail.js
@@ -1,18 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { Box, Typography, Paper, Grid } from '@mui/material';
+import { Box, Typography, Paper, Grid, ImageList, ImageListItem,List, ListItemText, ListItem } from '@mui/material';
 import ReactPlayer from 'react-player';
-import { List, ListItem, ListItemText } from '@mui/material';
-
-const CustomListItems = ({ items }) => (
-  <Box component="ul" sx={{ padding: 0, margin: 0, listStyleType: 'disc', marginLeft: 3 }}>
-    {items.map((item, index) => (
-      <Box component="li" key={index} sx={{ marginBottom: 1 }}>
-        <Typography variant="body2">{item}</Typography>
-      </Box>
-    ))}
-  </Box>
-);
 
 function ProjectDetail({ projects }) {
   const { projectId } = useParams();
@@ -21,46 +10,78 @@ function ProjectDetail({ projects }) {
   if (!project) {
     return <Typography variant="h6" gutterBottom>Project not found</Typography>;
   }
-  const implementationDetails = project.implementation ? project.implementation.split('; ') : ["Not specified"];
-  const technologiesUsed = project.technologies ? project.technologies.split('; ') : ["Not specified"];
-  // メディア要素のレンダリング（動画または画像）
-  const renderMedia = (type, url) => {
-    if (type === 'video') {
-      return <ReactPlayer url={url} controls width="100%" />;
-    } else {
-      return <img src={url} alt={project.title} style={{ width: '100%', height: 'auto', objectFit: 'cover', maxHeight: '60vh' }} />;
-    }
-  };
 
-  return (
-    <Box sx={{ width: '100%', overflow: 'hidden' }}>
-      <Grid container spacing={2} sx={{ marginTop: 2 }}>
-        <Grid item xs={12}>
-          {project.mediaUrl && renderMedia(project.mediaType, project.mediaUrl)}
-        </Grid>
-        <Grid item xs={12}>
-          <Paper elevation={6} sx={{ padding: 3 }}>
+  // Hero画像のレンダリング
+  const renderHeroImage = (url) => (
+    <img src={url} alt={project.title} style={{ width: '100%', objectFit: 'cover', maxHeight: '60vh' }} />
+  );
+
+  // メディア要素のレンダリング（動画または画像）
+  const renderMedia = (url) => (
+    <ReactPlayer url={url} controls width="100%" style={{ margin: 'auto' }} />
+  );
+
+  // 画像ギャラリーのレンダリング
+  const renderImageGallery = (images) => (
+    <ImageList cols={3} gap={8}>
+      {images.map((img, index) => (
+        <ImageListItem key={index}>
+          <img src={img} alt={`Gallery image ${index + 1}`} loading="lazy" style={{ width: '100%' }} />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
+   // プロジェクトの情報をリスト形式で表示
+   const renderInformationList = (title, data) => (
+    <ListItem>
+      <ListItemText primary={title} secondary={data.map(line => <Typography component="span" display="block">{line}</Typography>)} />
+    </ListItem>
+  );
+
+  function PaperSection({project}){
+    return (
+      <Paper elevation={6} sx={{ padding: 3, margin: 2 }}>
             <Typography variant="h3" component="h1" gutterBottom>
               {project.title}
             </Typography>
             <List>
-              <ListItem>
-                <ListItemText primary="概要" secondary={project.description} />
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="使用技術" secondary={<CustomListItems items={technologiesUsed} />} />
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="実装内容" secondary={project.implementation || "Not specified"} />
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="工夫点" secondary={project.innovations || "Not specified"} />
-              </ListItem>
-              <ListItem>
-                <ListItemText primary="学んだ点" secondary={project.lessonsLearned || "Not specified"} />
-              </ListItem>
+              {renderInformationList("概要", [project.description])}
+              {renderInformationList("使用技術", [project.technologies || "Not specified"])}
+              {renderInformationList("実装内容", project.implementation ? project.implementation.split(';') : ["Not specified"])}
+              {renderInformationList("工夫点", project.innovations ? project.innovations.split(';') : ["Not specified"])}
+              {renderInformationList("学んだ点", project.lessonsLearned ? project.lessonsLearned.split(';') : ["Not specified"])}
             </List>
-          </Paper>
+            {project.videoUrl && renderMedia(project.videoUrl)}
+      </Paper>
+    //   <Paper elevation={6} sx={{ padding: 3 }}>
+    //   <Typography variant="h3" component="h1" gutterBottom>
+    //     {project.title}
+    //   </Typography>
+    //   <Typography variant="body1" paragraph>
+    //     {project.description}
+    //   </Typography>
+    //   <Typography variant="body1" paragraph>
+    //     使用技術: {project.technologies || "Not specified"}
+    //   </Typography>
+    //   {/* Additional information */}
+    // </Paper>
+    )
+  }
+
+  return (
+    <Box sx={{ width: '100%', overflow: 'hidden', p: 0 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          {project.mediaUrl && renderHeroImage(project.mediaUrl)}
+        </Grid>
+        <Grid item xs={12}>
+          <PaperSection project={project}/>
+        </Grid>
+        <Grid item xs={12} md={8}>
+          {project.images && renderImageGallery(project.images)}
+        </Grid>
+        <Grid item xs={12} md={4}>
+          {/* {project.videoUrl && renderMedia(project.videoUrl)} */}
         </Grid>
       </Grid>
     </Box>
