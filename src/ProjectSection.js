8c5696e6b88e2879303a16d892782d7cf0494f76
agoyr <yamauchi.ryoga116@gmail.com>
2024-05-14 23:30:08 +0900

    fix: fix filter func at ProjectSection.js

--- src/ProjectSection.js
+++ src/ProjectSection.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Grid, Button, Menu, MenuItem } from '@mui/material';
+import { Grid, Button, Menu, MenuItem, Typography, Box } from '@mui/material';
 import ProjectCard from './ProjectCard';
 
 function ProjectSection({ projects }) {
@@ -24,31 +24,74 @@ function ProjectSection({ projects }) {
   });
 
   return (
-    <div id="projects">
-      <Button
-        aria-controls="simple-menu"
-        aria-haspopup="true"
-        onClick={handleClick}
-      >
-        Filter Projects
-      </Button>
-      <Menu
-        id="simple-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={() => handleMenuItemClick('All')}>All</MenuItem>
-        <MenuItem onClick={() => handleMenuItemClick('Web')}>Web</MenuItem>
-        <MenuItem onClick={() => handleMenuItemClick('Mobile')}>Mobile</MenuItem>
-      </Menu>
-      <Grid container justifyContent="center" alignItems="center" spacing={2} sx={{ p: 4 }}>
+    // <div id="projects">
+    //   <Box display="flex" justifyContent="space-between" alignItems="center" mb={2}>
+    //   <Typography variant="h4" component="h2" >
+    //     Project Section
+    //   </Typography>
+    //   <Button
+    //     aria-controls="simple-menu"
+    //     aria-haspopup="true"
+    //     onClick={handleClick}
+    //     variant="text"
+    //     // color="primary"
+    //     // sx={{ mb: 2 }}
+    //   >
+    //     Filter Projects
+    //   </Button>
+    //   <Menu
+    //     id="simple-menu"
+    //     anchorEl={anchorEl}
+    //     keepMounted
+    //     open={Boolean(anchorEl)}
+    //     onClose={handleClose}
+    //   >
+    //     <MenuItem onClick={() => handleMenuItemClick('All')}>All</MenuItem>
+    //     <MenuItem onClick={() => handleMenuItemClick('Web')}>Web</MenuItem>
+    //     <MenuItem onClick={() => handleMenuItemClick('Mobile')}>Mobile</MenuItem>
+    //   </Menu>
+    //   </Box>
+    //   <Grid container justifyContent="center" alignItems="center" spacing={2} sx={{ p: 4 }}>
+    //     {filteredProjects.map((project, index) => (
+    //       <ProjectCard key={index} {...project} />
+    //     ))}
+    //   </Grid>
+    // </div>
+    <Box id="projects">
+      <Box display="flex" justifyContent="space-between" alignItems="center" mb={2} sx={{ mt: 8, mb: 0, mx: 'auto', maxWidth: 800, px: 2 }}>
+        <Typography variant="h4" component="h2">
+          Projects
+        </Typography>
+        <Button
+          aria-controls="simple-menu"
+          aria-haspopup="true"
+          onClick={handleClick}
+          variant="text"
+        >
+          Filter Projects
+        </Button>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+        >
+          <MenuItem onClick={() => handleMenuItemClick('All')}>All</MenuItem>
+          <MenuItem onClick={() => handleMenuItemClick('Game')}>Game</MenuItem>
+          <MenuItem onClick={() => handleMenuItemClick('NativeApp')}>NativeApp</MenuItem>
+          <MenuItem onClick={() => handleMenuItemClick('Web')}>Web</MenuItem>
+          <MenuItem onClick={() => handleMenuItemClick('Research')}>Research</MenuItem>
+        </Menu>
+      </Box>
+      {/* <Box sx={{ width: '100%' }}> */}
+      <Grid container justifyContent="center" alignItems="center" spacing={2} sx={{ px: 4,py:2 }}>
         {filteredProjects.map((project, index) => (
           <ProjectCard key={index} {...project} />
         ))}
       </Grid>
-    </div>
+      {/* </Box> */}
+    </Box>
   );
 }
 
